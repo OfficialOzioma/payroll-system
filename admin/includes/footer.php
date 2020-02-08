fd73e433f3e30d629ad8caafcbceb90a6a0ee467
Ozioma <dikep15@gmail.com>
2020-02-09 00:34:19 +0100

    payroll system
    Signed-off-by: Ozioma <dikep15@gmail.com>

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Brought To You By <a href="https://code-projects.org/">Ozsoft</a></b>
+    </div>
+    <strong>Copyright &copy; <?php echo date('Y');?> Attendance and Payroll System </strong>
+</footer>
