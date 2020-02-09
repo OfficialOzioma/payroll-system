12c37d787862aec558cac00e50a1f41f5093f5f6
Dike Pascal Ozioma <dikep15@gmail.com>
2020-02-09 01:33:07 +0100

    Updated conn.php

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'apsystem');
+	$conn = new mysqli('sql9.freemysqlhosting.net', 'sql9321961', 'CPgKt87rlX', 'sql9321961');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
